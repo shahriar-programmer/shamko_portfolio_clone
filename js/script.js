8d5a7ec348048310befa852ef1d8b8e08b2eedea
Shahriar Ahmed Shovon <shovonshahriar01@gmail.com>
2021-01-25 16:23:13 +0600

    Upload Everyfile

---
+++ js/script.js
@@ -0,0 +1,14 @@
+var circle = document.querySelector('circle');
+var radius = circle.r.baseVal.value;
+var circumference = radius * 2 * Math.PI;
+
+circle.style.strokeDasharray = `${circumference} ${circumference}`;
+circle.style.strokeDashoffset = `${circumference}`;
+
+function setProgress(percent) {
+  const offset = circumference - percent / 100 * circumference;
+  circle.style.strokeDashoffset = offset;
+}
+
+const input = document.querySelector('input');
+setProgress(input.value);
